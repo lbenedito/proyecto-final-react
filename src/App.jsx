05421d33ee8a3209ea60c989c4061a5fb812ee4f
lbenedito <106201641+lbenedito@users.noreply.github.com>
2022-09-22 21:54:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+import Itemlistcontainer from './components/itemlistcontainer';
+import Navbar from './components/navbar';
+
+const App = () => {
+    return (
+        <div>
+         <Navbar/>,
+         
+         <Itemlistcontainer/>
+        </div>
+      
+       
+    );
+    
+}
+
+export default App;
+
